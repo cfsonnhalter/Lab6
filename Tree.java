26796fec943fe8770f35579747013465df426091
Chris Sonnhalter <cfsonnhalter@gmail.com>
2021-03-05 16:32:24 +0000

    done w bugs 2.0

--- Tree.java
+++ Tree.java
@@ -1,4 +1,4 @@
-private class Tree {
+class Tree {
 
   {
     String treeName;
@@ -7,73 +7,57 @@ private class Tree {
     String leafColor;
   }
 
-  Tree() 
-  {
+  Tree() {
     treeName = "";
     treeType = "";
     leavesFall = false;
     leafColor = "";
   }
 
-  Tree(String aTreeName, String aTreeType, boolean aLeavesFall, String aLeafColor)
-  {
+  Tree(String aTreeName, String aTreeType, boolean aLeavesFall, String aLeafColor) {
     treeName = aTreeName;
     treeType = aTreeType;
     leavesFall = aLeavesFall;
     leafColor = aLeafColor;
   }
 
-  public void setName(String theName){
-    this.name = theName;
+  public void setName(String aTreeName) {
+    name = treeName;
   }
 
-  public void setType(String theType){
-    this.name = theType;
+  public void setType(String aTreeType) {
+    name = treeType;
   }
 
-  public void setFall(boolean theFall){
-    this.name = theFall;
+  public void setFall(boolean aLeavesFall) {
+    name = leavesFall;
 
   }
 
-  public void setColor(String theColor){
-    this.name = theColor;
+  public void setColor(String aLeafColor) {
+    name = leafColor;
   }
 
-  public String getName(){
-    return tName;
+  public String getName() {
+    return treeName;
   }
 
-  public String getType(){
-    return tType;
+  public String getType() {
+    return treeType;
   }
 
-  public String getFall(){
-    return tFall;
+  public String getFall() {
+    return leavesFall;
   }
 
-  public String getColor(){
-    return tColor;
+  public String getColor() {
+    return leafColor;
   }
 
-  String print()
-  {
-    System.out.println("This is a " + getName.theName + ". It is a " + getType.theType + " and it's leaves are currently " + getColor.theColor + ". It " + (getFall.theFall == true ? "does" : "does not") + " lose it's leaves in the winter.");
+  String print() {
+    System.out.println("This is a " + getName.treeName + ". It is a " + getType.treeType
+        + " and it's leaves are currently " + getColor.leafColor + ". It "
+        + (getFall.leavesFall == true ? "does" : "does not") + " lose it's leaves in the winter.");
   }
-
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
